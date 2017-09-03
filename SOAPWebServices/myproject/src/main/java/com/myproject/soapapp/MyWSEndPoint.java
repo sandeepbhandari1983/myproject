f2afe2a3ad1319bbe52c1fe2e1ec188fff51a48b
sandeepbhandari1983 <sandeepbhandari1983@gmail.com>
2017-09-03 21:18:32 +0530

    web service successfully created

--- SOAPWebServices/myproject/src/main/java/com/myproject/soapapp/MyWSEndPoint.java
+++ SOAPWebServices/myproject/src/main/java/com/myproject/soapapp/MyWSEndPoint.java
@@ -7,19 +7,30 @@ import org.springframework.ws.soap.SoapHeader;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import  com.liverestaurant.orderservice.schema.*;
-@Endpoint 
+@Endpoint
 public class MyWSEndPoint 
 {
 
-    @PayloadRoot(localPart = "PlaceOrderRequest", namespace = "http://www.liverestaurant.com/OrderService/schema")       
+    @PayloadRoot(localPart = "cancelOrderRequest", namespace = "http://www.liverestaurant.com/OrderService/schema")       
 	@ResponsePayload
-  public PlaceOrderResponse order(@RequestPayload PlaceOrderRequest placeOrderRequest)
+  public CancelOrderResponse cancelOrder(@RequestPayload CancelOrderRequest placeOrderRequest)
+  {   
+CancelOrderResponse response= new CancelOrderResponse();
+  
+  System.out.println("Test");
+  response.setCancelled(false);
+  return response;
+  }
+
+      @PayloadRoot(localPart = "placeOrderRequest", namespace = "http://www.liverestaurant.com/OrderService/schema")       
+	@ResponsePayload
+  public PlaceOrderResponse placeOrder(@RequestPayload PlaceOrderRequest placeOrderRequest)
   {   
 PlaceOrderResponse response= new PlaceOrderResponse();
   
   System.out.println("Test");
-  response.setRefNumber("1");
+
   return response;
   }
-
+  
 }
